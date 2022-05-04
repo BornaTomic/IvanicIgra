6e71fb0f428e7c2ffa504fbdb550289c320013e2
Ivan710 <filipovicivan2005@gmail.com>
2022-05-04 16:17:49 +0200

    Ivan

--- MistOfWinterHaven/Assets/Scripts/SwordButton.cs
+++ MistOfWinterHaven/Assets/Scripts/SwordButton.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class SwordButton : MonoBehaviour
 {
+    GameObject sword;
+    GameObject border;
+    public bool isSword = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        border = GameObject.FindGameObjectWithTag("Border");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void GetSword()
+    {
+        sword.GetComponent<SpriteRenderer>().sortingLayerID = 3;
+        border.SetActive(true);
+        isSword = true;
+    }
+    public void RemoveSword()
+    {
+        sword.GetComponent<SpriteRenderer>().sortingLayerID = 0;
+        border.SetActive(false);
+        isSword = false;
     }
 }
